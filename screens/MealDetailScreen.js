991aff349ca7fff685fea86fadcb30c09d9414c2
Naveen Rawat <naveen.rawat@sprint.com>
2020-11-15 15:27:50 +0530

    meal details screen added

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 
-export default function MealDetailScreen() {
+import { MEALS } from "../data/dummy-data";
+
+export default function MealDetailScreen({ navigation }) {
+  const mealId = navigation.getParam("mealId");
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
   return (
     <View>
-      <Text>Meal Detail Screen</Text>
+      <Text>{selectedMeal.title}</Text>
     </View>
   );
 }
 
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  return {
+    headerTitle: selectedMeal.title,
+  };
+};
 const style = StyleSheet.create({});
